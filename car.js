d35bd5b588ea5e6d91d91c654984014ae9acd4eb
Kris <kharonek@yahoo.com>
2022-08-07 16:42:30 -0300

    Added border and traffic collisions

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x, y, width, height){
+    constructor(x, y, width, height, controlType, maxSpeed=3){
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,24 +7,28 @@ class Car{
 
         this.speed = 0;
         this.acc = 0.2;
-        this.maxSpeed = 3
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
 
         this.angle = 0;
         this.damaged = false;
 
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        if (controlType != "DUMMY"){
+          this.sensor = new Sensor(this);
+        }
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         if (!this.damaged) {
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assesDamage(roadBorders);
+            this.damaged = this.#assesDamage(roadBorders, traffic);
         }
-        
-        this.sensor.update(roadBorders);
+
+        if (this.sensor) {
+          this.sensor.update(roadBorders);
+        }
     }
 
     #move(){
@@ -35,9 +39,9 @@ class Car{
         if (this.controls.reverse){
             this.speed -= this.acc;
         }
-        
 
-     // speed cap   
+
+     // speed cap
         if (this.speed > this.maxSpeed){
             this.speed = this.maxSpeed;
         }
@@ -54,7 +58,7 @@ class Car{
         }
 
         if (Math.abs(this.speed)<this.friction){this.speed = 0;}
-        
+
         if (this.speed != 0){
                 const flip = this.speed>0?1:-1;
             if (this.controls.left){
@@ -64,17 +68,22 @@ class Car{
                 this.angle -= 0.03*flip;
             }
         }
-        
+
 
         this.x -= Math.sin(this.angle)*this.speed;
         this.y -= Math.cos(this.angle)*this.speed;
     }
 
-    #assesDamage(roadBorders){
+    #assesDamage(roadBorders, traffic){
         for (let i = 0; i < roadBorders.length; i++) {
             if (polyIntersect(this.polygon, roadBorders[i])){
                 return true;
-            }            
+            }
+        }
+        for (let i = 0; i < traffic.length; i++) {
+            if (polyIntersect(this.polygon, traffic[i].polygon)){
+                return true;
+            }
         }
         return false;
     }
@@ -83,7 +92,7 @@ class Car{
         const points = [];
         const rad = Math.hypot(this.width, this.height)/2;
         const alpha = Math.atan2(this.width, this.height);
-       
+
         points.push({
             x:this.x-Math.sin(this.angle-alpha)*rad,
             y:this.y-Math.cos(this.angle-alpha)*rad
@@ -112,16 +121,19 @@ class Car{
             ctx.fillStyle="gray";
         }
         else{
-            this.fillStyle="black";
+            ctx.fillStyle="black";
         }
+
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
 
         for (let i = 1; i < this.polygon.length; i++) {
-            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);    
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
         }
         ctx.fill();
-        
-        this.sensor.draw(ctx)
+
+        if (this.sensor) {
+          this.sensor.draw(ctx)
+        }
     }
 }
